dd8d81c85289152b2ad13161cd6cd699e1dbba67
Olivier Huin <olih@users.noreply.github.com>
2026-04-25 20:22:16 +0100

    Adds required flag

--- doc/design-meta/examples/args-admin.ts
+++ doc/design-meta/examples/args-admin.ts
@@ -8,16 +8,38 @@ export const schemaString: ArgsCommandSchema = adminArgs
   .adminOnly()
   .string('min-chars', stringValidations.minChars(10))
   .string('max-chars', stringValidations.maxChars(20))
-  .string('enum', stringValidations.enum(['green', 'orange', 'red']))
+  .string('enum', stringValidations.enum(['green', 'orange', 'red']), [
+    ['schema', 'string', '--enum', 'green,orange,red', '--required'],
+  ])
   .build();
 
 export const washStartSchema: ArgsCommandSchema = adminArgs
   .command(['wash', 'start'])
   .adminOnly()
-  .string('mode', stringValidations.enum(['normal', 'delicate', 'whites']))
+  .string('mode', stringValidations.enum(['normal', 'delicate', 'whites']), [
+    ['schema', 'string', '--enum', 'normal,delicate,whites', '--required'],
+  ])
   .boolean('extra-rinse')
-  .number('spin', numberValidations.int())
-  .tuple('range', [numberValidations.int(), numberValidations.int()])
+  .number('spin', numberValidations.int(), [
+    ['schema', 'number', '--int', '--required'],
+  ])
+  .tuple(
+    'range',
+    [numberValidations.int(), numberValidations.int()],
+    [
+      [
+        'schema',
+        'number',
+        '--tuple',
+        '0',
+        '--int',
+        '--tuple',
+        '1',
+        '--int',
+        '--required',
+      ],
+    ],
+  )
   .string('add', stringValidations.alpha(), [
     ['schema', 'repeatable', '--min-length', '1', '--max-length', '5'],
   ])
@@ -29,5 +51,7 @@ export const washStartSchema: ArgsCommandSchema = adminArgs
   .number('dose', numberValidations.int(), [
     ['schema', 'repeatable', '--min-length', '1', '--max-length', '3'],
   ])
-  .string('temp', stringValidations.enum(['cold', 'warm', 'hot']))
+  .string('temp', stringValidations.enum(['cold', 'warm', 'hot']), [
+    ['schema', 'string', '--enum', 'cold,warm,hot', '--required'],
+  ])
   .build();
