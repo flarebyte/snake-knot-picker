c2347a67996c6c58fe41dccfc3b1adb7c4cea890
Olivier Huin <olih@users.noreply.github.com>
2026-04-25 07:03:41 +0100

    Tidy email string example

--- doc/design-meta/examples/string-chain.ts
+++ doc/design-meta/examples/string-chain.ts
@@ -1,12 +1,10 @@
 import type { StringValidation, StringValidationChain } from './common';
 import { stringFormatters } from './formatters';
-import { numberValidations } from './number';
 import { stringValidations } from './string';
 
 export const normalizedEmailChain: StringValidationChain = stringValidations
   .chain()
   .pipe(stringValidations.matchesFormatter(stringFormatters.trim()))
-  .pipe(stringValidations.number(numberValidations.int()))
   .pipe(stringValidations.minChars(10))
   .pipe(stringValidations.maxChars(40))
   .pipe(stringValidations.email());
